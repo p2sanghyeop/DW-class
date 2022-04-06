2200b98988c5e77c4979bec1144a4ed012ada2d3
p2sanghyeop <px346987@naver.com>
2022-04-06 23:35:16 +0900

    커피 계산

--- JAVA/22.04.06/cafe.java
+++ JAVA/22.04.06/cafe.java
@@ -1,6 +1,4 @@
 package 상속;
-
-
 class coffee{
 	int count;
 	
@@ -9,18 +7,26 @@ class coffee{
 	}
 	public void buyCoffee(int count) {
 		 this.count+=count ;
-		if(count>0 && count==10) {
+		 System.out.println("커피"+count+"구입");
+		if(0<count && count<=10) {
 			System.out.println("브론즈");
 		}
-		if(count>10 && count==20) {
+		if(10<this.count && this.count<=20) {
 			System.out.println("실버");
 		}
-		if(count>20 && count==30) {
+		if(count>20) {
 			System.out.println("골드");
 		}
 	}
+	public void drinkCoffee(int count){ //먹은 사과
+		if(this.count >= count){
+			System.out.println("커피를 "+count+"개 마심");
+			this.count -= count;
+		}else{
+			System.out.println("커피부족"+count+"개 만큼 못마심");
+		}
 }
-
+}
 public class cafe {
 
 	public static void main(String[] args) {
@@ -28,7 +34,9 @@ public class cafe {
 		System.out.println("현재커피"+coffee.getCoffee());
 		coffee.buyCoffee(20);
 		if(coffee.getCoffee()>0) {
-			System.out.println("커피"+ coffee.getCoffee()+"결제");
+			System.out.println("마신커피"+ coffee.getCoffee());
+			coffee.buyCoffee(10);
+			System.out.println("마신커피"+ coffee.getCoffee());
 		}
 	
 	}
